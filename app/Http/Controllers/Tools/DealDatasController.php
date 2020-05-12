546821cef489ae8532062a71e410d7cc4cf40f1d
puyunjun <15084341790@163.com>
2020-05-13 01:14:00 +0800

    'puyunjun'

--- app/Http/Controllers/Tools/DealDatasController.php
+++ app/Http/Controllers/Tools/DealDatasController.php
@@ -7,6 +7,7 @@ use App\Models\Reservation;
 use App\Servers\PhpSpreadSheet;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class DealDatasController extends Controller
@@ -16,6 +17,9 @@ class DealDatasController extends Controller
     public function index(Request $request)
     {
 
+        //$this->addStudentTeacherData();
+        $this->addStudentStu();
+        dd(1);
         /*$jsonData = file_get_contents('noImportDeviceRecord.json');
         $jsonAllData = file_get_contents('noImportRecord.json');
 
@@ -63,15 +67,69 @@ class DealDatasController extends Controller
 
 
     //处理数据
+    //最笨的方法，处理8000多次,或者用任务队列跑
     public function dealOldData($data = [])
     {
         foreach ($data as $item){
             //学生姓名
             $studentName = $item['user'];
 
+            //先匹配学生姓名
+
             //导师姓名
             $tutor = $item['tutor'];
 
         }
+    }
+    //表格数据添加
+    public function addStudentTeacherData()
+    {
+        //
+        $sql = "SELECT * FROM dj_user_all
+                    where  school_number not  in
+                (SELECT `no` FROM cq_student ) AND teacher_id = 0 AND role >1
+                ";
+        $res = DB::select(DB::raw($sql));
+        //将没有添加到student 表的导师添加到数据库中
+        $insertTeacherData = [];
+        foreach ($res as $value){
+            //查找当前老师对应的学院id
+            $schoolId = DB::table('school')->where('name',$value->school_name)->value('id');
+            $insertTeacherData[]=[
+                'no'=>$value->school_number,
+                'name'=>$value->user_name,
+                'mobile'=>$value->mobile,
+                'school_id'=>$schoolId,
+                'email'=>$value->email
+            ];
+        }
+        DB::table('student')->insert($insertTeacherData);
+        //dd($insertTeacherData);
+    }
+
+    public function addStudentStu()
+    {
+        $sql = "SELECT * FROM dj_user_student_view where  school_number not in (SELECT `no` FROM cq_student )";
+
+        $res = DB::select(DB::raw($sql));
+
+        $insertData = [];
+        foreach ($res as $item){
+            //查找当前对应学生导师工号对应的student表id
+            $teachNum = $item->teacherNumber;
+
+            $teacherId = DB::table('student')->where('no',$teachNum)->value('id');
+
+            $schoolId = DB::table('school')->where('name',$item->school_name)->value('id');
+            $insertData[] = [
+                'no'=>$item->school_number,
+                'name'=>$item->user_name,
+                'mobile'=>$item->mobile,
+                'teacher_id'=>$teacherId ? $teacherId : 0,
+                'school_id'=>$schoolId,
+                'email'=>$item->email
+            ];
+        }
+        dd($insertData);
     }
 }
